8fe4955d4008587577fd2e2a76ed2db2158a13b0
p0daru <102858022+p0daru@users.noreply.github.com>
2024-01-05 18:44:06 +0200

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Pogorila Daria on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
